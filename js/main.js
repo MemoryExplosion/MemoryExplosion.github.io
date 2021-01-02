0ca142b8240ab698b4068d0668a603fd3397b938
13083513833@163.com <15601597255a>
2021-01-02 17:47:36 +0800

    Site updated: 2021-01-02 17:47:33

--- js/main.js
+++ js/main.js
@@ -1,79 +1,38 @@
-var alphaDust = function () {
+window.addEventListener('DOMContentLoaded', () => {
 
-    var _menuOn = false;
+  KEEP.themeInfo = {
+    author: 'XPoet',
+    name: 'Keep',
+    version: KEEP.theme_config.version,
+    repository: 'https://github.com/XPoet/hexo-theme-keep'
+  }
 
-    function initPostHeader() {
-        $('.main .post').each(function () {
-            var $post = $(this);
-            var $header = $post.find('.post-header.index');
-            var $title = $post.find('h1.title');
-            var $readMoreLink = $post.find('a.read-more');
+  // print theme info
+  KEEP.utils.printThemeInfo();
 
-            var toggleHoverClass = function () {
-                $header.toggleClass('hover');
-            };
+  // init scroll
+  KEEP.utils.registerWindowScroll();
 
-            $title.hover(toggleHoverClass, toggleHoverClass);
-            $readMoreLink.hover(toggleHoverClass, toggleHoverClass);
-        });
-    }
+  // toggle show tools list
+  KEEP.utils.toggleShowToolsList();
 
-    function _menuShow () {
-        $('nav a').addClass('menu-active');
-        $('.menu-bg').show();
-        $('.menu-item').css({opacity: 0});
-        TweenLite.to('.menu-container', 1, {padding: '0 40px'});
-        TweenLite.to('.menu-bg', 1, {opacity: '0.92'});
-        TweenMax.staggerTo('.menu-item', 0.5, {opacity: 1}, 0.3);
-        _menuOn = true;
+  // global font adjust
+  KEEP.utils.globalFontAdjust();
 
-        $('.menu-bg').hover(function () {
-            $('nav a').toggleClass('menu-close-hover');
-        });
-    }
+  KEEP.utils.contentAreaWidthAdjust();
 
-    function _menuHide() {
-        $('nav a').removeClass('menu-active');
-        TweenLite.to('.menu-bg', 0.5, {opacity: '0', onComplete: function () {
-            $('.menu-bg').hide();
-        }});
-        TweenLite.to('.menu-container', 0.5, {padding: '0 100px'});
-        $('.menu-item').css({opacity: 0});
-        _menuOn = false;
-    }
+  // comment
+  KEEP.utils.goComment();
 
-    function initMenu() {
+  // init page height handle
+  KEEP.utils.initPageHeightHandle();
 
-        $('nav a').click(function () {
-            if(_menuOn) {
-                _menuHide();
-            } else {
-                _menuShow();
-            }
-        });
+  // init first screen height
+  KEEP.utils.initFirstScreenHeight();
 
-        $('.menu-bg').click(function (e) {
-            if(_menuOn && e.target === this) {
-                _menuHide();
-            }
-        });
-    }
+  // big image viewer handle
+  KEEP.utils.imageViewer();
 
-    function displayArchives() {
-        $('.archive-post').css({opacity: 0});
-        TweenMax.staggerTo('.archive-post', 0.4, {opacity: 1}, 0.15);
-    }
-
-    return {
-        initPostHeader: initPostHeader,
-        initMenu: initMenu,
-        displayArchives: displayArchives
-    };
-}();
-
-
-$(document).ready(function () {
-    alphaDust.initPostHeader();
-    alphaDust.initMenu();
-    alphaDust.displayArchives();
+  // set how long age in home article block
+  KEEP.utils.setHowLongAgoInHome();
 });
